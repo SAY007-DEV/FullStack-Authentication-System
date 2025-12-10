c5a86d3a3a1c4fc877ffac06f01d8ed78ec892c6
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2025-12-10 11:11:30 +0530

    otp controller added

---
+++ back-end/controller/Otpconteoller.js
@@ -0,0 +1,26 @@
+import Otp from "../Models/otpModels";
+
+import crypto from "crypto";
+
+
+export const otp = async (req, res)=>{
+    const {email} = req.body;
+    try {
+        const existinguser = Otp.findOne({email});
+        if(!existinguser){
+            return res.status(404).json({message: "User not found, please register"});
+        }
+      
+       const generatedOtp = crypto.randomInt(300000, 999999).toString();
+       Otp.otp = generatedOtp;
+       otp.createdAt = Date.now();
+         await Otp.save();
+
+
+
+        res.status(200).json({ email, otp: generatedOtp, message: "OTP generated successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
