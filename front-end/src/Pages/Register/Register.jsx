e9d4f507af8684a9a12efade4f771a7fc2e724f8
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2025-12-16 10:00:41 +0530

    register page added

--- front-end/src/Pages/Register/Register.jsx
+++ front-end/src/Pages/Register/Register.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useState } from "react";
 
 function Register() {
+ const[name, setName]=useState("");
+const[email, setEmail]=useState("");
+const[password, setPassword]=useState("");
+
   return (
     <div className="form-container sign-up-container">
       <form>
@@ -19,10 +24,11 @@ function Register() {
         </div>
 
         <span>or use your email for registration</span>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Sign Up</button>
+        <input type="text" placeholder="Name" onChange={(e)=>setName(e.target.value)} />
+        <input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="Password"onChange={(e)=>setPassword(e.target.value)} />
+       
+        <button >Sign Up</button>
       </form>
     </div>
   );
