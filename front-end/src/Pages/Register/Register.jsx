464c3e8fb8147423b9bf8a1e73d58ecc46e18f46
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2025-12-16 17:24:14 +0530

    dashboard added

--- front-end/src/Pages/Register/Register.jsx
+++ front-end/src/Pages/Register/Register.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 function Register() {
  const[name, setName]=useState("");
 const[email, setEmail]=useState("");
 const[password, setPassword]=useState("");
 
+async function handleSubmit(e){
+  e.preventDefault();
+  // console.log(name, email, password);
+  try {
+    const response = await axios.post('http://localhost:8000/api/register', {
+      name,
+      email,
+      password,
+    });
+    console.log("Success:", response.data);
+   
+  } catch (error) {
+    
+    console.error("Error:", error.response ? error.response.data : error.message);
+  }
+}
   return (
     <div className="form-container sign-up-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Create Account</h1>
 
         <div className="social-container">
