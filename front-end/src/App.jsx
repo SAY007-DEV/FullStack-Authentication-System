3280ee4d74b3245e3900a26929b238eba15f9714
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2025-12-18 11:21:05 +0530

    login page bug fixed

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,7 +6,7 @@ import AuthContainer from './Pages/Register/AuthContainer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
--- front-end/src/Pages/Register/Login.jsx
+++ front-end/src/Pages/Register/Login.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+
+const[email, setEmail]=useState("");
+const[password, setPassword]=useState("");
+
+async function handleSubmit(e){
+  e.preventDefault();
+ 
+  try {
+    const response = await axios.post('http://localhost:8000/api/register', {
+      email,
+      password,
+    });
+    console.log("Success:", response.data);
+   
+  } catch (error) {
+    
+    console.error("Error:", error.response ? error.response.data : error.message);
+  }
+}   
+
+
+
   return (
     <div className="form-container sign-in-container">
       <form>
@@ -19,10 +41,10 @@ function Login() {
         </div>
 
         <span>or use your account</span>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} />
+        <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
         <a href="#">Forgot your password?</a>
-        <button>Sign In</button>
+        <button onClick={handleSubmit}>Sign In</button>
       </form>
     </div>
   );
